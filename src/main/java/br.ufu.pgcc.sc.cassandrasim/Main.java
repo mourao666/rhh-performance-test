49edc38632c310c58cf0e3e992c09283054d3d80
mourao666 <mourao@lntb-mul037628.network.ctbc>
2016-03-25 12:23:41 -0300

    First commit with resources

---
+++ src/main/java/br.ufu.pgcc.sc.cassandrasim/Main.java
@@ -0,0 +1,9 @@
+package br.ufu.pgcc.sc.cassandrasim;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
---
+++ src/main/resources/lib/generate_keys.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# coding: utf-8
+
+if __name__ == "__main__":
+
+    import random
+
+    f = open('../keys', 'w')
+
+    for it in range(10000):
+        array = random.sample(range(0, 99), 50)
+        f.write(' '.join(str(x) for x in array))
+        f.write('\n')
+
+    f.close()
